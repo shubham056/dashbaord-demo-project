6febce01356e9f469000a42934b6854be716f6a6
Shubham <shubham.k@cwsinfotech.com>
2022-05-10 23:05:54 +0530

    aaded isActive in sidebar

--- src/components/Opportunities/index.js
+++ src/components/Opportunities/index.js
@@ -1,33 +1,63 @@
-import React from 'react'
+import React from 'react';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
 
-export default function OpportunitiesTable({ opportunities }) {
 
+export default function OpportunitiesTable({ isLoading, opportunities }) {
+    console.log(isLoading)
+    console.log("opportunities", opportunities)
     let opportunitiesJsx;
-    if (typeof opportunities != "undefined" && opportunities !== null && opportunities.length > 0) {
+    if (isLoading) {
         opportunitiesJsx = opportunities.map(item => (
             <tr>
-                <td>{item.id}</td>
-                <td>{item.company_name} <span>{item.company_seriies}</span></td>
-                <td>{item.round}</td>
-                <td>{item.round_size}</td>
-                <td>{item.total_investments} </td>
-                <td>{item.total_paid}</td>
-                <td>{item.closing_date} </td>
+                <td>{ isLoading ? <Skeleton /> : item.id}</td>
+                <td>{ isLoading ? <Skeleton /> : <> {item.company_name} <span>{item.company_seriies}</span> </>}  </td>
+                <td>{ isLoading ? <Skeleton /> : item.round}</td>
+                <td>{ isLoading ? <Skeleton /> : item.round_size}</td>
+                <td>{ isLoading ? <Skeleton /> : item.total_investments} </td>
+                <td>{ isLoading ? <Skeleton /> : item.total_paid}</td>
+                <td>{ isLoading ? <Skeleton /> : item.closing_date} </td>
                 <td>
-                    <label className="switch">
+                    {
+                        isLoading ? <Skeleton /> : <label className="switch">
                         <input type="checkbox" defaultChecked />
                         <span className="slider round" />
                     </label>
+                    }
+                    
                 </td>
             </tr>
         ))
+        
     } else {
-        opportunitiesJsx = (
-            <tr>
-                <td>No opportunities found</td>
-            </tr>
-        )
+        if (typeof opportunities != "undefined" && opportunities !== null && opportunities.length > 0) {
+            opportunitiesJsx = opportunities.map(item => (
+                <tr>
+                    <td>{item.id}</td>
+                    <td>{item.company_name} <span>{item.company_seriies}</span></td>
+                    <td>{item.round}</td>
+                    <td>{item.round_size}</td>
+                    <td>{item.total_investments} </td>
+                    <td>{item.total_paid}</td>
+                    <td>{item.closing_date} </td>
+                    <td>
+                        <label className="switch">
+                            <input type="checkbox" defaultChecked />
+                            <span className="slider round" />
+                        </label>
+                    </td>
+                </tr>
+            ))
+        } else {
+            opportunitiesJsx = (
+                <tr>
+                    <td>No opportunities found</td>
+                </tr>
+            )
+        }
     }
+
+
 
     return (
         <div className="card">
--- src/pages/Opportunities/index.js
+++ src/pages/Opportunities/index.js
@@ -4,25 +4,31 @@ import Layout from '../../components/Layout';
 import { JSON_API } from '../../utils/Constants';
 import { Helmet } from "react-helmet";
 import OpportunitiesTable from '../../components/Opportunities';
+import Companies from '../../components/Opportunities/Companies';
+import CreateDeal from '../../components/Opportunities/CreateDeal';
 
 export default function Opportunities() {
-
+    const [isLoading, setisLoading] = useState(true);
     const [opportunitiesData, setOpportunitiesData] = useState([]);
 
     const getOpportunitiesData = async () => {
         axios.get(`${JSON_API}/opportunities_data`)
             .then(function (response) {
                 setOpportunitiesData(response.data)
+                setisLoading(false)
             })
             .catch(function (error) {
                 // handle error
+                setisLoading(true)
                 console.log(error);
             })
     };
 
     useEffect(() => {
         getOpportunitiesData();
-    }, [opportunitiesData])
+    }, [])
+
+  
 
     return (
         <>
@@ -34,68 +40,31 @@ export default function Opportunities() {
             <Layout
                 pageHeading={'Opportunities'}
             />
-            {/* Main Content Area */}
+
             <div className="main">
                 <section className="topsec">
                     <div className="container">
                         <div className="row">
                             <div className="col-12 col-sm-12 col-md-9 col-lg-9 col-xl-9 pr-2">
-                                <OpportunitiesTable opportunities={opportunitiesData} />
+
+                                <OpportunitiesTable 
+                                   isLoading ={isLoading}
+                                    opportunities={opportunitiesData}
+                                 />
+
                             </div>
                             <div className="col-12 col-sm-12 col-md-3 col-lg-3 col-xl-3 pl-2">
-                                <div className="card">
-                                    <div className="card-body">
-                                        <h3>Add an Investor</h3>
-                                        <form>
-                                            <div className="form-group">
-                                                <label>Name</label>
-                                                <input type="text" name="Iname" className="form-control" placeholder="Investor Name" />
-                                            </div>
-                                            <div className="form-group">
-                                                <label>Email</label>
-                                                <input type="email" name="Iname" className="form-control" placeholder="Email Address" />
-                                            </div>
-                                            <div className="form-group text-right">
-                                                <button type="button" className="btn btn-primary">+ Add</button>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
-                                <div className="card mt-3">
-                                    <div className="card-body">
-                                        <h3>Invite Investors</h3>
-                                        <form>
-                                            <div className="form-group">
-                                                <label>Email</label>
-                                                <input type="text" name="sname" className="form-control" placeholder="Search Name Here" />
-                                                <input type="text" name="Iname" className="form-control" placeholder="Email" />
-                                            </div>
-                                            <div className="form-group">
-                                                <label>Select Round</label>
-                                                <select className="form-control">
-                                                    <option>Monsoon Creditech</option>
-                                                    <option>Monsoon Creditech</option>
-                                                    <option>Monsoon Creditech</option>
-                                                </select>
-                                                <select className="form-control">
-                                                    <option>Series I</option>
-                                                    <option>Series I</option>
-                                                    <option>Series I</option>
-                                                </select>
-                                            </div>
-                                            <div className="form-group text-right">
-                                                <button type="button" className="btn btn-primary">+ Add</button>
-                                            </div>
-                                        </form>
-                                    </div>
-                                </div>
+
+                                <CreateDeal />
+
+                                <Companies />
+
                             </div>
+
                         </div>
                     </div>
                 </section>
             </div>
-
-
         </>
     )
 }
